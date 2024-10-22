5057dfb3963d81d74e409f2aa3a03a58b468af7e
Tylko-jedno <diogkek@gmail.com>
2024-10-22 15:24:39 +0300

    first master commit

---
+++ Lab08_1it_str/Lab08_1it_str.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int Count(const string& str) {
+    int k = 0;
+    for (char c : str) {
+        if (c == '+' || c == '-' || c == '=') {
+            k++;
+        }
+    }
+    return k;
+}
+
+string Change(const string& str) {
+    string newStr;
+    for (char c : str) {
+        if (c == '+' || c == '-' || c == '=') {
+            newStr += "**";
+        }
+        else {
+            newStr += c;
+        }
+    }
+    return newStr;
+}
+
+int main() {
+    string str;
+    cout << "Enter string:" << endl;
+    getline(cin, str);
+
+    cout << "String contained " << Count(str) << " symbols: + - =" << endl;
+
+    string modifiedStr = Change(str);
+    cout << "Modified string (result): " << modifiedStr << endl;
+
+    return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,28 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab08_1it_str/Lab08_1it_str.cpp"
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestProject
+{
+    TEST_CLASS(UnitTestProject)
+    {
+    public:
+
+        // Тест для функції Count
+        TEST_METHOD(TestCount)
+        {
+            string str1 = "abc+def=gh-ijk";
+            Assert::AreEqual(3, Count(str1)); // Три символи '+', '-', '='
+        }
+
+        TEST_METHOD(TestChange)
+        {
+            string str1 = "abc+def=gh-ijk";
+            string expected1 = "abc**def**gh**ijk";
+            Assert::AreEqual(expected1, Change(str1));
+        }
+    };
+}
